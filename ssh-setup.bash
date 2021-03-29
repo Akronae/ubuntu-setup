3ea69dc6fb87ecdc7d95ac340652518ba3affbe6
Alexande Daubricourt <alexandre@daubricourt.com>
2021-03-29 12:44:56 +0200

    Update ssh-setup.bash

--- ssh-setup.bash
+++ ssh-setup.bash
@@ -1,2 +1,4 @@
-mkdir .ssh
-cp authorized_keys .ssh/authorized_keys
+BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+mkdir -p ~/.ssh
+cp $BASH_DIR/authorized_keys ~/.ssh/authorized_keys

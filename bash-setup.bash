6a8c60f663eb51a0453cc9fc006852d868849a47
Alexande Daubricourt <alexandre@daubricourt.com>
2021-03-29 12:56:16 +0200

    Update bash-setup.bash

--- bash-setup.bash
+++ bash-setup.bash
@@ -3,4 +3,4 @@ BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cp $BASH_DIR/~/.bash_aliases ~/.bash_aliases
 cp $BASH_DIR/~/.bashrc ~/.bashrc
 
-. ~/.bashrc
+$(. ~/.bashrc)

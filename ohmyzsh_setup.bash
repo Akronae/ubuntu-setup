e4dad5bd788e19b2a52978e0e312151bbe3f7237
Alexande Daubricourt <alexandre@daubricourt.com>
2021-11-17 14:09:31 +0100

    Update ohmyzsh_setup.bash

--- ohmyzsh_setup.bash
+++ ohmyzsh_setup.bash
@@ -1,5 +1,6 @@
 sudo apt install curl wget git zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sed -i 's/_THEME=\"robbyrussell\"/_THEME=\"pygmalion\"/g' ~/.zshrc && source ~/.zshrc
 sudo chsh -s $(which zsh) $(whoami)
 sudo sed -i 's/_THEME=\"robbyrussell\"/_THEME=\"pygmalion\"/g' ~/.zshrc && source ~/.zshrc
 sudo sed -i .bak 's/_THEME=\"robbyrussell\"/_THEME=\"pygmalion\"/g' ~/.zshrc && source ~/.zshrc

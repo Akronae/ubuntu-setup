e3512a6eda8e10b4a6680d5d96d799ce57a11531
Alexande Daubricourt <alexandre@daubricourt.com>
2022-01-17 13:41:11 +0100

    Update docker_setup.bash
    Add aveg for more entropy. Causes docker-compose to hang up

--- docker_setup.bash
+++ docker_setup.bash
@@ -10,7 +10,7 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io haveged
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose

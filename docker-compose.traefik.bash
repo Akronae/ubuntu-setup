b543bb8deb43511c0d9d2aa630985cc6731bf87a
Alexande Daubricourt <alexandre@daubricourt.com>
2022-01-26 15:27:49 +0100

    Update docker-compose.traefik.bash

--- docker-compose.traefik.bash
+++ docker-compose.traefik.bash
@@ -1,6 +1,8 @@
 FILE_DIR=$(dirname "$0")
 cd $FILE_DIR
 
+sudo mkdir -p /docker-volumes/letsencrypt
+
 docker network create -d overlay traefik-public
 docker swarm init
 docker stack deploy -c=docker-compose.traefik.yml --with-registry-auth loadbalancer

08681dc1f06129dc001497ac10280ba2c12f3041
Alexande Daubricourt <alexandre@daubricourt.com>
2022-01-26 15:24:33 +0100

    Update docker-compose.traefik.bash

--- docker-compose.traefik.bash
+++ docker-compose.traefik.bash
@@ -1,6 +1,6 @@
 FILE_DIR=$(dirname "$0")
 cd $FILE_DIR
 
-network create -d overlay traefik-public
+docker network create -d overlay traefik-public
 docker swarm init
 docker stack deploy -c=docker-compose.traefik.yml --with-registry-auth loadbalancer

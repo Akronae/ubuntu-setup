21310f83a28e453c2343489f8fedf52e87544f9b
Alexande Daubricourt <alexandre@daubricourt.com>
2022-06-14 13:10:19 +0200

    Update ohmyzsh_setup.bash

--- ohmyzsh_setup.bash
+++ ohmyzsh_setup.bash
@@ -1,3 +1,4 @@
 sudo apt install curl wget git zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 omz theme set pygmalion
+echo "$(echo $'zstyle \':omz:update\' mode auto' | cat - ~/.zshrc)" > ~/.zshrc

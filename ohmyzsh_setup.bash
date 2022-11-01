ee2ba4558c7742f9fbf5e56f7c65826aefe65329
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-11-01 10:00:01 +0000

    m

--- ohmyzsh_setup.bash
+++ ohmyzsh_setup.bash
@@ -4,7 +4,7 @@ echo "$(echo $'zstyle \':omz:update\' mode auto' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'del () { mv "$@" /tmp; }' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'alias cat="batcat --paging=never --style=plain"' | cat - ~/.zshrc)" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "oh-my-zsh installed, run the following commands to finish up: \033[0;35m
-omz theme set pygmalion
-omz plugin enable nvm
+/bin/zsh -i -c "\
+omz theme set pygmalion &&\
+omz plugin enable nvm &&\
 omz plugin enable zsh-autosuggestions"

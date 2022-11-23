f18531f9faf1404c22e1cebbd185654ced598244
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-11-24 00:18:56 +0100

    cleanbr git alias

--- git.bash
+++ git.bash
@@ -1,2 +1,3 @@
 git config --global user.name "Alexandre Daubricourt"
 git config --global user.email "alexandre@daubricourt.com"
+git config --global alias.cleanbr "! git branch -d \`git branch --merged | grep -v '^*\\|main\\|master\\|staging\\|devel'\`"

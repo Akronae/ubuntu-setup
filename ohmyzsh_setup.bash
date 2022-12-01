45ba48c0a0685cb19503c1640d3ae260696738cb
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-12-01 17:30:51 +0100

    Update ohmyzsh_setup.bash

--- ohmyzsh_setup.bash
+++ ohmyzsh_setup.bash
@@ -2,7 +2,7 @@ sudo apt install curl wget git zsh bat
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "$(echo $'zstyle \':omz:update\' mode auto' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'del () { mv "$@" /tmp; }' | cat - ~/.zshrc)" > ~/.zshrc
-echo "$(echo $'alias cat="batcat --paging=never --style=plain' | cat - ~/.zshrc)" > ~/.zshrc
+echo "$(echo $'alias cat="batcat --paging=never --style=plain"' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'export PATH=$HOME/bin:/usr/local/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 /bin/zsh -i -c "\

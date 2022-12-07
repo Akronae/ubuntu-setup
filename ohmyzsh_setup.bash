9243d6bd8c88ae06dcd0271023a826ff53a63d59
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-12-07 11:36:59 +0100

    Update ohmyzsh_setup.bash

--- ohmyzsh_setup.bash
+++ ohmyzsh_setup.bash
@@ -3,9 +3,16 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "$(echo $'zstyle \':omz:update\' mode auto' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'del () { mv "$@" /tmp; }' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'alias cat="batcat --paging=never --style=plain"' | cat - ~/.zshrc)" > ~/.zshrc
-echo "$(echo $'export PATH=$HOME/bin:/usr/local/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
+echo "$(echo $'export PATH=/usr/local/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
+echo "$(echo $'export PATH=$HOME/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
+echo "$(echo $'export PATH=$HOME/.local/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-/bin/zsh -i -c "\
-omz theme set pygmalion &&\
-omz plugin enable nvm &&\
-omz plugin enable zsh-autosuggestions"
+
+echo "\
+run the following commands:
+\033[0;31m
+omz theme set pygmalion
+omz plugin enable nvm
+omz plugin enable zsh-autosuggestions
+\033[0m
+"

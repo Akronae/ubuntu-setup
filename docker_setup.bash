3ae53f8da19a30cb0aa49e4691d306b15bee507b
Alexandre Daubricourt <alexandre@daubricourt.com>
2023-08-10 11:46:29 +0200

    Update docker_setup.bash

--- docker_setup.bash
+++ docker_setup.bash
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg
@@ -14,6 +14,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 tput setaf 201; echo "Docker is up and fresh on this machine ✨"

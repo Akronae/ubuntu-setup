42d2a2d5e5fc04e423f5e67098d7c0e87d55eda4
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-04-01 17:01:33 +0200

    got wokeddd

--- git.bash
+++ git.bash
@@ -1,3 +1,4 @@
 git config --global user.name "Alexandre Daubricourt"
 git config --global user.email "alexandre@daubricourt.com"
 git config --global alias.cleanbr "! git branch -d \`git branch --merged | grep -v '^*\\|main\\|master\\|staging\\|devel'\`"
+git config --global init.defaultBranch main

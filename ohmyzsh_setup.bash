7a40d22a1895bdaa83c0e1779a26a096da89fe72
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-07-05 09:25:32 +0200

    Update ohmyzsh_setup.bash

--- ohmyzsh_setup.bash
+++ ohmyzsh_setup.bash
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt install curl wget git zsh bat -y
+sudo apt install curl wget git zsh bat fzf -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "$(echo $'zstyle \':omz:update\' mode auto' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'zstyle \':omz:alpha:lib:git\' async-prompt no' | cat - ~/.zshrc)" > ~/.zshrc
@@ -11,6 +11,8 @@ echo "$(echo $'export PATH=$HOME/.local/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'export PATH=/usr/local/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autopair
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab 
 
 /bin/zsh -c "
 export ZSH=${ZSH:-/$USER/.oh-my-zsh}
@@ -18,5 +20,7 @@ export ZSH=${ZSH:-/$USER/.oh-my-zsh}
 omz theme set pygmalion
 omz plugin enable zsh-autosuggestions
 omz plugin enable zsh-autopair
+omz plugin enable zsh-syntax-highlighting
+omz plugin enable fzf-tab
 zsh
 "

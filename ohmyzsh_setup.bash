8e18ca63dbfb0249599a7df3b899c09db92c55eb
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-12-13 11:48:39 +0100

    Update ohmyzsh_setup.bash

--- ohmyzsh_setup.bash
+++ ohmyzsh_setup.bash
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt install curl wget git zsh bat fzf -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "$(echo $'zstyle \':omz:update\' mode auto' | cat - ~/.zshrc)" > ~/.zshrc
-echo "$(echo $'zstyle \':omz:alpha:lib:git\' async-prompt no' | cat - ~/.zshrc)" > ~/.zshrc
+#echo "$(echo $'zstyle \':omz:alpha:lib:git\' async-prompt no' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'del () { mv "$@" /tmp; }' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'alias cat="batcat --paging=never --style=plain"' | cat - ~/.zshrc)" > ~/.zshrc
 echo "$(echo $'export PATH=$HOME/bin:$PATH' | cat - ~/.zshrc)" > ~/.zshrc
